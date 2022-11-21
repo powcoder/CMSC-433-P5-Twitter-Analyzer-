90cb437eee7e3e532cdf3c1ca98d9317dc850dbf
powcoder <powcoder@163.com>
2022-11-21 20:54:39 +0800

    CMSC 433 P5 Twitter Analyzer

---
+++ src/cmsc433/p5/TrendingParameter.java
@@ -0,0 +1,16 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+/**
+ * DO NOT MODIFY!
+ */
+package cmsc433.p5;
+
+/**
+ * The parameter on which to perform the map reduce.
+ *
+ */
+public enum TrendingParameter {
+	USER, TWEET, HASHTAG, HASHTAG_PAIR;
+}
